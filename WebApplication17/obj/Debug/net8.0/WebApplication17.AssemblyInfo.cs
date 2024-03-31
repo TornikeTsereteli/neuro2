e0af6a16156a70fad0b808ba05f54460e745b4c8
TornikeTsereteli <tornike.tsereteli2003@gmail.com>
2024-03-31 15:24:47 +0400

    dummy

--- WebApplication17/obj/Debug/net8.0/WebApplication17.AssemblyInfo.cs
+++ WebApplication17/obj/Debug/net8.0/WebApplication17.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication17")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2229a0d1f7384cd6ffe06133936ba66f67b8137e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication17")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication17")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
